46135cbcf363ac90f43610197338bd1d171dfca4
Cassiano Marinello <crmarinello@gmail.com>
2026-05-28 12:13:28 -0300

    fix: hide TanStack Router Devtools in production

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -34,7 +34,7 @@ function RootComponent() {
       </main>
       <AppFooter />
       <LgpdConsentBanner />
-      <TanStackRouterDevtools position="bottom-right" />
+      {import.meta.env.DEV && <TanStackRouterDevtools position="bottom-right" />}
     </div>
   )
 }
